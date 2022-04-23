e3d502b420f377a4c598ad2fc95bd7b9d534859c
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-24 00:35:04 +0800

    Added Room Service

---
+++ src/main/java/com/mancala/api/services/RoomService.java
@@ -0,0 +1,27 @@
+package com.mancala.api.services;
+
+import java.util.UUID;
+
+import com.mancala.api.models.Player;
+import com.mancala.api.models.Room;
+import com.mancala.api.respository.RoomRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+    @Autowired
+    public RoomRepository roomRepository;
+
+    public Room createRoom(Player player) {
+        Player[] players = new Player[] { player };
+        Room room = new Room();
+        room.setId(UUID.randomUUID().toString());
+        room.setName("name");
+        room.setPlayers(players);
+        roomRepository.save(room);
+        return room;
+    }
+}
