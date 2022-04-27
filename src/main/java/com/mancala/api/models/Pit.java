28ce0788cdd794e4c378483fb9dd2d86d456fe6c
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-28 00:51:56 +0800

    Added Pit Model

---
+++ src/main/java/com/mancala/api/models/Pit.java
@@ -0,0 +1,40 @@
+package com.mancala.api.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Pit {
+
+    private Integer id;
+    private Integer stones;
+    private boolean isHouse;
+
+    @JsonIgnore
+    public boolean isEmpty() {
+        return stones == 0;
+    }
+
+    public void addStones() {
+        stones++;
+    }
+
+    public void addStones(Integer i) {
+        stones = stones + i;
+    }
+
+    public Integer countStones() {
+        return stones;
+    }
+
+    @Override
+    public String toString() {
+        return id.toString() + ":" + countStones().toString();
+    }
+}
