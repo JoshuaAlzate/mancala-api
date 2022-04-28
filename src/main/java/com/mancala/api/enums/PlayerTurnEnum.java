af47735c0a6e52df808a30b7c11d1678d53c6c70
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-28 23:34:31 +0800

    Added Player Turn Enum

---
+++ src/main/java/com/mancala/api/enums/PlayerTurnEnum.java
@@ -0,0 +1,16 @@
+package com.mancala.api.enums;
+
+public enum PlayerTurnEnum {
+    FIRST_PLAYER, SECOND_PLAYER;
+
+    public static PlayerTurnEnum togglePlayerTurn(PlayerTurnEnum currentPlayerTurn) {
+        switch (currentPlayerTurn) {
+            case FIRST_PLAYER:
+                return SECOND_PLAYER;
+            case SECOND_PLAYER:
+                return FIRST_PLAYER;
+            default:
+                throw new RuntimeException("Invalid player turn");
+        }
+    }
+}
