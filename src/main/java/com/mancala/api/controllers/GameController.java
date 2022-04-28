a893d94f7fa8a1ef9508fc5bacf9aab53c65586d
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-28 18:13:05 +0800

    Added Game Controller and Service

---
+++ src/main/java/com/mancala/api/controllers/GameController.java
@@ -0,0 +1,47 @@
+package com.mancala.api.controllers;
+
+import com.mancala.api.models.Game;
+import com.mancala.api.services.GameService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/game")
+public class GameController {
+    private final GameService gameService;
+
+    @GetMapping("/getAll")
+    Iterable<Game> getAllRooms() {
+        return gameService.getAllGames();
+    }
+
+    @PutMapping("/create")
+    ResponseEntity<Game> createGame(@RequestBody String roomID) {
+        log.info("Create game session with respect to room ID: {}", roomID);
+        return ResponseEntity.ok(gameService.createGame(roomID));
+    }
+
+    @DeleteMapping("/deleteAll")
+    ResponseEntity<Boolean> deleteRooms() {
+        log.info("Delete all rooms: {}");
+        return ResponseEntity.ok(gameService.deleteGames());
+    }
+
+    @GetMapping("/getDetails/{gameID}")
+    ResponseEntity<Game> getGameDetails(@PathVariable("gameID") String gameID) {
+        log.info("Getting room details of id: {}", gameID);
+        return ResponseEntity.ok(gameService.getGameDetails(gameID));
+    }
+}
---
+++ src/main/java/com/mancala/api/services/GameService.java
@@ -0,0 +1,50 @@
+package com.mancala.api.services;
+
+import java.lang.StackWalker.Option;
+import java.util.Optional;
+
+import com.mancala.api.exceptions.GameException;
+import com.mancala.api.models.Game;
+import com.mancala.api.models.Room;
+import com.mancala.api.respository.GameRepository;
+import com.mancala.api.respository.RoomRepository;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+    private final RoomRepository roomRepository;
+
+    public Iterable<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+    public Game createGame(String roomID) {
+        Optional<Room> optionalRoom = roomRepository.findById(roomID);
+        optionalRoom.orElseThrow(() -> new GameException("The room ID provided is not existing"));
+
+        Room room = optionalRoom.get();
+        Game game = new Game();
+        game.setRoom(room);
+        gameRepository.save(game);
+        return game;
+    }
+
+    public boolean deleteGames() {
+        gameRepository.deleteAll();
+        return true;
+    }
+
+    public Game getGameDetails(String roomID) {
+        Optional<Game> optionalGame = gameRepository.findById(roomID);
+        optionalGame.orElseThrow(() -> new GameException("The game ID provided is not existing"));
+
+        Game game = optionalGame.get();
+        return game;
+    }
+}
