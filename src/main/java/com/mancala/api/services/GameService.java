3f9a2906e2574e1855a582ca5d441bc436d1fffe
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-29 14:31:05 +0800

    Added Game Logic

--- src/main/java/com/mancala/api/services/GameService.java
+++ src/main/java/com/mancala/api/services/GameService.java
@@ -1,10 +1,12 @@
 package com.mancala.api.services;
 
-import java.lang.StackWalker.Option;
 import java.util.Optional;
 
+import com.mancala.api.enums.PlayerTurnEnum;
 import com.mancala.api.exceptions.GameException;
 import com.mancala.api.models.Game;
+import com.mancala.api.models.Pit;
+import com.mancala.api.models.Player;
 import com.mancala.api.models.Room;
 import com.mancala.api.repository.GameRepository;
 import com.mancala.api.repository.RoomRepository;
@@ -21,6 +23,7 @@ public class GameService {
     @Autowired
     private final GameRepository gameRepository;
     private final RoomRepository roomRepository;
+    private final PlayerService playerService;
 
     public Iterable<Game> getAllGames() {
         return gameRepository.findAll();
@@ -44,11 +47,79 @@ public class GameService {
         return true;
     }
 
-    public Game getGameDetails(String roomID) {
-        Optional<Game> optionalGame = gameRepository.findById(roomID);
+    public Game getGameDetails(String gameID) {
+        Optional<Game> optionalGame = gameRepository.findById(gameID);
         optionalGame.orElseThrow(() -> new GameException("The game ID provided is not existing"));
 
         Game game = optionalGame.get();
         return game;
     }
+
+    // Game Logic
+    public void playerTurn(String gameID, String playerID, Integer pitIndex) {
+        Game game = getGameDetails(gameID);
+        Player player = playerService.getPlayer(playerID);
+        Pit selectedPit = game.getPitByIndex(pitIndex);
+
+        Integer selectedPitStones = selectedPit.getStones();
+        selectedPit.setStones(0);
+
+        for (int x = selectedPitStones; x != 0; x--) {
+            pitIndex = getNextPitIndex(pitIndex++);
+            Pit pit = game.getPitByIndex(pitIndex);
+            if (pit.isHouse()) {
+                if ((game.getPlayerTurn().equals(PlayerTurnEnum.FIRST_PLAYER) && pitIndex == 6) ||
+                        (game.getPlayerTurn().equals(PlayerTurnEnum.SECOND_PLAYER) && pitIndex == 13)) {
+                    pit.addStone();
+                }
+            } else {
+                pit.addStone();
+            }
+        }
+
+        // Capture
+        Pit lastPit = game.getPitByIndex(pitIndex);
+        capturePit(pitIndex, game);
+
+        if (!lastPit.isHouse()) {
+            switchPlayerTurn(game, lastPit);
+        }
+
+        gameRepository.save(game);
+
+    }
+
+    private Integer getNextPitIndex(Integer currentPitIndex) {
+        currentPitIndex = (currentPitIndex + 1) % 14;
+        return currentPitIndex;
+    }
+
+    private Pit capturePit(Integer pitIndex, Game game) {
+        Pit lastPit = game.getPitByIndex(pitIndex);
+        Pit oppositePit = game.getPitByIndex(12 - pitIndex >= 0 ? 12 - pitIndex : 7);
+        if (!lastPit.getId().equals(Game.FIRST_PLAYER_HOUSE) &&
+                !lastPit.getId().equals(Game.SECOND_PLAYER_HOUSE) &&
+                lastPit.getStones() == 1 &&
+                ((game.getPlayerTurn().equals(PlayerTurnEnum.FIRST_PLAYER) && lastPit.getId() < 7) ||
+                        (game.getPlayerTurn().equals(PlayerTurnEnum.SECOND_PLAYER) && lastPit.getId() > 8))) {
+
+            Pit housePit = pitIndex < Game.FIRST_PLAYER_HOUSE ? game.getPitByIndex(Game.FIRST_PLAYER_HOUSE)
+                    : game.getPitByIndex(Game.SECOND_PLAYER_HOUSE);
+            housePit.addStones(oppositePit.getStones() + lastPit.getStones());
+            lastPit.setStones(0);
+            oppositePit.setStones(0);
+        }
+
+        return lastPit;
+    }
+
+    private void switchPlayerTurn(Game game, Pit lastPin) {
+        if (!((game.getPlayerTurn().equals(PlayerTurnEnum.FIRST_PLAYER)
+                && lastPin.getId().equals(Game.FIRST_PLAYER_HOUSE)) ||
+                (game.getPlayerTurn().equals(PlayerTurnEnum.SECOND_PLAYER)
+                        && lastPin.getId().equals(Game.SECOND_PLAYER_HOUSE)))) {
+            game.setPlayerTurn(PlayerTurnEnum.togglePlayerTurn(game.getPlayerTurn()));
+        }
+    }
+
 }
